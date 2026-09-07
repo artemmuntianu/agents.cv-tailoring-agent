bcac1dfd42690a049db6601398a2a3bf5cc5dd6a
artemmuntianu <artemmuntianu@gmail.com>
2026-09-07 13:00:59 +0100

    c2

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-GEMINI_API_KEY = ""
+GEMINI_API_KEY = "AQ.Ab8RN6LxUlHdW4Dfu5CIX_uN3E1jY_hfHwXRx3adt6nWZTLAAA"
 if GEMINI_API_KEY:
     os.environ["GEMINI_API_KEY"] = GEMINI_API_KEY
 
-MODEL_NAME = "gemini-2.5-flash"
+MODEL_NAME = "gemini-3.5-flash"
 MAX_REVISIONS = 3
 RENDER_DPI = 70
 
